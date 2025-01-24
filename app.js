436d2c52a7051b619a02f44fb19a26429284f14c
Marcos Santos <markeka.mar@gmail.com>
2025-01-24 19:35:47 -0400

    atualizar exercicios

--- app.js
+++ app.js
@@ -23,10 +23,40 @@ function somanumero() {
   let numero1 = prompt('Entre com um número inteiro');
   let numero2 = prompt('Entre com um segundo número inteiro');
   let resultado = parseInt(numero1) + parseInt(numero2);
-  alert(`A soma dos número é ${resultado}.`);
+  alert(`A soma dos númers é ${resultado}.`);
 }
 
 btn.addEventListener('click', consoleLog);
 btn2.addEventListener('click', alerta);
 btn3.addEventListener('click', perguntaCidade);
 btn4.addEventListener('click', somanumero);
+
+
+
+// function dobrarNumero(numerod) {
+//   let resultado = numerod * 2;
+//   return resultado;
+// }
+
+// function calcularMedia(numero1, numero2, numero3) {
+//   let media = (numero1 + numero2 + numero3) / 3;
+//   return media;
+// }
+
+// function encontrarMaior(numero1, numero2) { 
+//   if (numero1 > numero2) 
+//   { return numero1; } else { return numero2; } 
+// }
+
+// function olanome() {
+//   let nome = prompt('Qual é o seu nome');
+//   console.log(`Olá, ${nome}!`);
+// }
+
+// function olamundo() {
+//   console.log('Olá mundo...');
+// }
+
+// olanome();
+// olamundo();
+
